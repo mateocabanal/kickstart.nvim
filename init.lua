037dd0a7d5de7ca94bcf6e463a9ff585f04643c0
Mateo Cabanal <mateo@mateocabanal.ca>
2023-03-10 11:03:38 -0800

    added stuff

--- init.lua
+++ init.lua
@@ -315,7 +315,7 @@ require('telescope').setup {
             ['<C-d>'] = false,
       },
     },
-  }, 
+  },
 }
 
 -- Enable telescope fzf native, if installed
@@ -430,10 +430,10 @@ local on_attach = function(_, bufnr)
   nmap('r', vim.lsp.buf.rename, '[R]ename')
   nmap('c', vim.lsp.buf.code_action, '[C]ode Action')
 
-  nmap('d', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('r', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('I', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap('gD', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
@@ -442,7 +442,7 @@ local on_attach = function(_, bufnr)
   nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
 
   -- Lesser used LSP functionality
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('gx', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
   nmap('wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
   nmap('wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
   nmap('wl', function()
